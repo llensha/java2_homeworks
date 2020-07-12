f0a831488c0615ab330592afac93ce3628ca01fd
llensha <llensha@mail.ru>
2020-07-12 13:00:47 +0300

    Homework_7

---
+++ server/Server.java
@@ -0,0 +1,77 @@
+package Lesson_7.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            AuthService.connect();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+            clients = new Vector<>();
+
+            while (true) {
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this, socket);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void subscribe(ClientHandler client) {
+        clients.add(client);
+    }
+
+    public void unsubscribe(ClientHandler client) {
+        clients.remove(client);
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    public void sendPrivateMsg(String nickFrom, String nickTo, String msg) {
+        for (ClientHandler o: clients) {
+            if (o.getNick().equalsIgnoreCase(nickTo)) {
+                o.sendMsg(nickFrom + " (private): " + msg);
+            }
+        }
+    }
+
+    public boolean isNickFree(String nick) {
+        for (ClientHandler o: clients) {
+            if (o.getNick().equals(nick)) return false;
+        }
+        return true;
+    }
+
+
+}
